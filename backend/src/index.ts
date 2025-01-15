28f8f68060175fdb1960aaf24a9acdd1f823ba84
Amaan Abdullah <amaantheone@gmail.com>
2025-01-15 17:20:00 +0530

    add signup and signin routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,25 +1,67 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign } from 'hono/jwt';
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  }
+}>();
 
-app.post('/api/v1/user/signup', (c) => {
-  return c.text('signup route')
-})
+app.post("/api/v1/user/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
 
-app.post('/api/v1/user/signin', (c) => {
-  return c.text('signin route')
-})
+  const body = await c.req.json();
 
-app.post('/api/v1/blog', (c) => {
-  return c.text('blog route')
-})
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password
+    }
+  })
 
-app.get('/api/v1/blog/:id', (c) => {
-  return c.text('blogId route')
-})
+  const token = await sign({ id: user.id}, c.env.JWT_SECRET);
 
-app.get('/api/v1/blog/bulk', (c) => {
-  return c.text('blog bulk route')
-})
+  return c.json({
+    jwt: token
+  });
+});
 
-export default app
+app.post("/api/v1/user/signin", async(c) => {
+  const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL	,
+	}).$extends(withAccelerate());
+
+	const body = await c.req.json();
+	const user = await prisma.user.findUnique({
+		where: {
+			email: body.email
+		}
+	});
+
+	if (!user) {
+		c.status(403);
+		return c.json({ error: "user not found" });
+	}
+
+	const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+	return c.json({ jwt });
+});
+
+app.post("/api/v1/blog", (c) => {
+  return c.text("blog route");
+});
+
+app.get("/api/v1/blog/:id", (c) => {
+  return c.text("blogId route");
+});
+
+app.get("/api/v1/blog/bulk", (c) => {
+  return c.text("blog bulk route");
+});
+
+export default app;
